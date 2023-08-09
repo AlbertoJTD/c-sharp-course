27b862502a8437a34ac84768428309cc01f132e6
AlbertoJTD <albertojtd@outlook.com>
2023-08-09 11:43:19 -0600

    constant variables

--- FirstApp/Program.cs
+++ FirstApp/Program.cs
@@ -6,13 +6,26 @@ namespace FirstApp
 	{
 		static void Main(string[] args)
 		{
-            Console.WriteLine("First number: ");
-			int n1 = int.Parse(Console.ReadLine());
+			//         Console.WriteLine("First number: ");
+			//int n1 = int.Parse(Console.ReadLine());
 
-            Console.WriteLine("Second number: ");
-			int n2 = int.Parse(Console.ReadLine());
+			//         Console.WriteLine("Second number: ");
+			//int n2 = int.Parse(Console.ReadLine());
 
-            Console.WriteLine($"Sum: {n1 + n2}");
+			//         Console.WriteLine($"Sum: {n1 + n2}");
+
+			const int VALUE1 = 10;
+			const int VALUE2 = 20;
+            Console.WriteLine("Values: {0} - {1}", VALUE1, VALUE2);
+
+			const double PI = 3.1416;
+            Console.WriteLine("Type the radius: ");
+			double radius = double.Parse(Console.ReadLine());
+
+			//double area = radius * radius * PI;
+			double area = Math.Pow(radius, 2) * PI;
+
+			Console.WriteLine($"The circle area is {area}");
         }
 	}
 }
