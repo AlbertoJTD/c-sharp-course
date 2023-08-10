a85e4d270fd5fee7c90d2cc6f29cea9bafe8e85c
AlbertoJTD <albertojtd@outlook.com>
2023-08-10 09:00:46 -0600

    Launch exceptions with throw

--- FirstApp/Program.cs
+++ FirstApp/Program.cs
@@ -7,11 +7,49 @@ namespace FirstApp
 	{
 		static void Main(string[] args)
 		{
-			checked // Verify if there is an error
+            Console.WriteLine("Type the number of the month: ");
+			int month = int.Parse(Console.ReadLine());
+			try
 			{
-				int num = int.MaxValue; // Gets the maximun value of an int type
-				int result = num + 30; // It should be out of int range
-				Console.WriteLine(result);
+				var result = MonthName(month);
+                Console.WriteLine(result);
+            }
+            catch (ArgumentOutOfRangeException e)
+			{
+                Console.WriteLine($"Error: {e.Message}");
+            }
+		}
+
+		public static string MonthName(int month)
+		{
+			switch (month)
+			{
+				case 1:
+					return "January";
+				case 2:
+					return "February";
+				case 3:
+					return "March";
+				case 4:
+					return "April";
+				case 5:
+					return "May";
+				case 6:
+					return "June";
+				case 7:
+					return "July";
+				case 8:
+					return "August";
+				case 9:
+					return "September";
+				case 10:
+					return "October";
+				case 11:
+					return "November";
+				case 12:
+					return "December";
+				default:
+					throw new ArgumentOutOfRangeException(nameof(month), "The month must be between 1-12");
 			}
 		}
 	}
