c945422d70d36aa76ebd209b3eb84eec36036c36
AlbertoJTD <albertojtd@outlook.com>
2023-08-10 17:56:11 -0600

    Default constructor

---
+++ FirstApp/Coordinate.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FirstApp
+{
+	class Coordinate
+	{
+        public Coordinate()
+        {
+        }
+
+        public Coordinate(int x, int y)
+        {
+            Console.WriteLine($"Coordinate: ({x},{y})");
+        }
+    }
+}
--- FirstApp/Program.cs
+++ FirstApp/Program.cs
@@ -7,59 +7,13 @@ namespace FirstApp
 	{
 		static void Main(string[] args)
 		{
-			Car car = new Car(); // Create a new object using the constructor
-            Console.WriteLine(car.getInfoCar());
-
-			Car car2 = new Car(3000, 1400); // Create a new object using the constructor
-			Console.WriteLine(car2.getInfoCar());
-
-			car2.setExtraInfo(true, "Red");
-            Console.WriteLine(car2.getExtras());
+            DoSomething();
         }
-	}
 
-	partial class Car
-	{
-		// Constructor
-		// Constructor Overloading
-		public Car() // Provide an initial state for all created objects
+		static void DoSomething()
 		{
-			wheels = 4;
-			height = 2300.5;
-			width = 1200;
-		}
-
-		public Car(double height, double width)
-		{
-			this.wheels = 4;
-			this.height = height;
-			this.width = width;
+			Coordinate pointA = new Coordinate();
+			Coordinate pointB = new Coordinate(10, 2);
 		}
 	}
-
-	partial class Car {
-		// Properties
-		private int wheels;
-		private double height;
-		private double width;
-		private bool airConditioner;
-		private string color;
-
-		// Getter -> Get info
-		public string getInfoCar()
-		{
-			return $"--- Info car ---\nWheels: 4\nHeight: {height}\nWidth: {width}";
-		}
-
-		public void setExtraInfo(bool airconditioner, string color)
-		{
-			this.airConditioner = airconditioner;
-			this.color = color;
-		}
-
-		public string getExtras()
-		{
-			return $"--- Extra info car ---\nAir Conditioner: {airConditioner}\nColor: {color}";
-		}
-    }
 }
