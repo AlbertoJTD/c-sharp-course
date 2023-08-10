04a45263a51a6e9887e6371c6ab509803e524615
AlbertoJTD <albertojtd@outlook.com>
2023-08-09 21:14:47 -0600

    Guess the random number

--- FirstApp/Program.cs
+++ FirstApp/Program.cs
@@ -7,17 +7,34 @@ namespace FirstApp
 	{
 		static void Main(string[] args)
 		{
-            Console.WriteLine("Do you want to get into a while loop?");
-			string result = Console.ReadLine();
+            Console.WriteLine("Guess the random number");
 
-			// Infinity loop
-			while (result != "no")
-			{
-				Console.WriteLine("While loop executing...");
+			Random random = new Random();
+			int randomNumber = random.Next(0, 100);
+            bool guessed = false;
+			int attemps = 0;
+
+            while (!guessed)
+            {
+                Console.WriteLine("Type the number: ");
+				int userValue = int.Parse(Console.ReadLine());
+
+				if (userValue > randomNumber)
+				{
+                    Console.WriteLine("The typed value is greather than the generated number\n");
+					attemps++;
+				}
+				else if (userValue < randomNumber)
+				{
+					Console.WriteLine("The typed value is less than the generated number\n");
+					attemps++;
+				}
+				else
+				{
+                    Console.WriteLine($"\n\nYou guessed the number: {randomNumber}, Attemps: {attemps}");
+					guessed = true;
+				}
 			}
-
-			Random num = new Random();
-            Console.WriteLine(num.Next(0, 100));
-        }
+		}
 	}
 }
