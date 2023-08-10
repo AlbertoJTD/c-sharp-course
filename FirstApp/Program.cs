42b682fbc4fae8de811f9ae917333068d05e91af
AlbertoJTD <albertojtd@outlook.com>
2023-08-09 21:30:24 -0600

    Do while loop

--- FirstApp/Program.cs
+++ FirstApp/Program.cs
@@ -7,34 +7,12 @@ namespace FirstApp
 	{
 		static void Main(string[] args)
 		{
-            Console.WriteLine("Guess the random number");
+			int num1 = 10;
 
-			Random random = new Random();
-			int randomNumber = random.Next(0, 100);
-            bool guessed = false;
-			int attemps = 0;
-
-            while (!guessed)
-            {
-				attemps++;
-
-				Console.WriteLine("Type the number: ");
-				int userValue = int.Parse(Console.ReadLine());
-
-				if (userValue > randomNumber)
-				{
-                    Console.WriteLine("The typed value is greather than the generated number\n");
-				}
-				else if (userValue < randomNumber)
-				{
-					Console.WriteLine("The typed value is less than the generated number\n");
-				}
-				else
-				{
-                    Console.WriteLine($"\n\nYou guessed the number: {randomNumber}, Attemps: {attemps}");
-					guessed = true;
-				}
-			}
+			do
+			{
+                Console.WriteLine("Hello world");
+            } while (num1 < 10);
 		}
 	}
 }
