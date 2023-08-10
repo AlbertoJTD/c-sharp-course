1565d6f5063f0f3c0f3070ecf4a99b06a1b2a5ca
AlbertoJTD <albertojtd@outlook.com>
2023-08-09 19:21:35 -0600

    If structure

--- FirstApp/Program.cs
+++ FirstApp/Program.cs
@@ -6,18 +6,10 @@ namespace FirstApp
 	{
 		static void Main(string[] args)
 		{
-            Console.WriteLine(Sum(7, 5));
-		}
+            int age = 16;
 
-		// Optional parameters
-		static double Sum(int a, double b, double c=0)
-		{
-			return a + b + c;
-		}
-
-		static double Sum(int a, double b)
-		{
-			return a + b;
+			if (age > 15) Console.WriteLine("More than 15 years old");
+			else Console.WriteLine("less than 15");
 		}
 	}
 }
