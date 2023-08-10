43f698aceeb444d1ef18964d1b8a6bd38cf09249
AlbertoJTD <albertojtd@outlook.com>
2023-08-10 15:47:19 -0600

    Encapsulatio and Accessors

--- FirstApp/Program.cs
+++ FirstApp/Program.cs
@@ -7,20 +7,45 @@ namespace FirstApp
 	{
 		static void Main(string[] args)
 		{
-			Circle circle = new Circle(); // New Circle object type
+			//Circle circle = new Circle(); // New Circle object type
 
-			double result = circle.CalculateArea(5);
-            Console.WriteLine(result);
+			//double result = circle.CalculateArea(5);
+   //         Console.WriteLine(result);
+
+
+			ConvertEuroToDollar convertMoney = new ConvertEuroToDollar();
+			//convertMoney.euro = 1.45;
+			convertMoney.ModifyEuroValue(1.45);
+			Console.WriteLine(convertMoney.Convert(50));
         }
 	}
 
 	class Circle
 	{
-		const double PI = 3.1416; // Property of Circle class
+		private const double PI = 3.1416; // Property of Circle class
 
 		public double CalculateArea(int radio) // Method class
 		{
 			return PI * Math.Pow(radio, 2);
 		}
     }
+
+	class ConvertEuroToDollar
+	{
+		public double euro = 0.9108;
+
+		public double Convert(double amount)
+		{
+			return amount * euro;
+		}
+
+		// Accessors
+		public void ModifyEuroValue(double newValue)
+		{
+			if (newValue > 0)
+			{
+				euro = newValue;
+			}
+		}
+	}
 }
