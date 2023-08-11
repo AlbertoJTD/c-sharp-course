6e15f15877b5b6bdc5e842f20c74252621a30849
AlbertoJTD <albertojtd@outlook.com>
2023-08-11 15:58:05 -0600

    Substitution principle

--- FirstApp/Program.cs
+++ FirstApp/Program.cs
@@ -10,12 +10,32 @@ namespace FirstApp
 			Human human = new Human("John Wick");
 			Gorilla gorilla = new Gorilla("Joe");
 
-			gorilla.Climb();
+			//gorilla.Climb();
 
-            Console.WriteLine(gorilla.getName());
-            Console.WriteLine(horse.getName());
-            Console.WriteLine(human.getName());
-        }
+			//Console.WriteLine(gorilla.getName());
+			//Console.WriteLine(horse.getName());
+			//Console.WriteLine(human.getName());
+
+			// ----- Substitucion principle -----
+			//Horse horse2 = new Mammal("Horse"); This is not possible
+
+			Mammal animal = new Horse("Horse"); // This is correct
+			Mammal person = new Human("Brayan"); // This is correct
+
+			Mammal animal2 = new Mammal("Animal2");
+			Horse horse2 = new Horse("Horse2");
+			animal2 = horse2;
+
+			Object myAnimal = new Horse("Tiger");
+			Object myPerson = new Horse("Jonny");
+			Object myMammal = new Horse("Wally");
+
+			Mammal[] myMammals = new Mammal[3];
+			myMammals[0] = animal;
+			myMammals[0] = person;
+			myMammals[0] = new Gorilla("King kong");
+
+		}
 	}
 
 	class Mammal
