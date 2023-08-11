85b94a4aec0f4510b06ebb9a35221819714d130c
AlbertoJTD <albertojtd@outlook.com>
2023-08-11 12:28:49 -0600

    Constructors and base

--- FirstApp/Program.cs
+++ FirstApp/Program.cs
@@ -6,17 +6,28 @@ namespace FirstApp
 	{
 		static void Main(string[] args)
 		{
-			Horse horse = new Horse();
-			Human human = new Human();
-			Gorilla gorilla = new Gorilla();
+			Horse horse = new Horse("Mustang");
+			Human human = new Human("John Wick");
+			Gorilla gorilla = new Gorilla("Joe");
 
 			gorilla.Climb();
+
+            Console.WriteLine(gorilla.getName());
+            Console.WriteLine(horse.getName());
+            Console.WriteLine(human.getName());
         }
 	}
 
 	class Mammal
 	{
-		public void Breath()
+		private string name;
+
+        public Mammal(string name)
+        {
+			this.name = name;
+        }
+
+        public void Breath()
 		{
             Console.WriteLine("I can breathe");
         }
@@ -25,11 +36,19 @@ namespace FirstApp
 		{
             Console.WriteLine("I am eating");
         }
+
+		public string getName()
+		{
+			return name;
+		}
 	}
 
 	class Horse : Mammal
 	{
-		public void Gallop()
+		// this calls the parent constructor ->  Mammal
+		public Horse(string horseName) : base(horseName) { }
+
+        public void Gallop()
 		{
 			Console.WriteLine("I am able to gallop...");
 		}
@@ -37,7 +56,9 @@ namespace FirstApp
 
 	class Human : Mammal
 	{
-		public void Think()
+        public Human(string humanName) : base(humanName) { }
+
+        public void Think()
 		{
 			Console.WriteLine("I am thinking");
 		}
@@ -45,7 +66,9 @@ namespace FirstApp
 
 	class Gorilla : Mammal
 	{
-		public void Climb()
+        public Gorilla(string gorillaName) : base(gorillaName) { }
+
+        public void Climb()
 		{
 			Console.WriteLine("I am climbing");
 		}
