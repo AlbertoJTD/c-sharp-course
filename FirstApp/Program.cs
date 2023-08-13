0e6b237f66077581a3f6df303e7a5f6434d7ab51
AlbertoJTD <albertojtd@outlook.com>
2023-08-13 15:51:24 -0600

    Remove code

--- FirstApp/Program.cs
+++ FirstApp/Program.cs
@@ -5,12 +5,7 @@ namespace FirstApp
 	{
 		static void Main(string[] args)
 		{
-			TrafficNotices trafficNotice = new TrafficNotices(); // default constructor
-			trafficNotice.ShowNotice();
 
-			TrafficNotices trafficNotice2 = new TrafficNotices("Police", "exceeding the maximum speed limit", "08/13/2023");
-            Console.WriteLine(trafficNotice2.GetDate());
-            trafficNotice2.ShowNotice();
 		}
 	}
 }
