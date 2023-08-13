08d52e753276ea66eaebc8e7c786f640394c97fe
AlbertoJTD <albertojtd@outlook.com>
2023-08-13 11:49:35 -0600

    Add whale class

--- FirstApp/Program.cs
+++ FirstApp/Program.cs
@@ -1,62 +1,105 @@
 ﻿using System;
-
 namespace FirstApp
 {
 	class Program
 	{
 		static void Main(string[] args)
 		{
-			Airplane airplane = new Airplane();
-			Car car = new Car();
+			Horse horse = new Horse("Mustang");
+			Human human = new Human("John Wick");
+			Gorilla gorilla = new Gorilla("Joe");
 
-			airplane.StartEngine();
-			airplane.Drive();
-			airplane.StopEngine();
+			Mammal animal = new Horse("Horse");
+			Mammal person = new Human("Brayan");
 
-			car.StartEngine();
-			car.Drive();
-			car.StopEngine();
+			Mammal[] myMammals = new Mammal[3];
+			myMammals[0] = animal;
+			myMammals[1] = person;
+			myMammals[2] = new Gorilla("King kong");
 
-            Console.WriteLine("\nPolymorphism in action");
-			Vehicle myVehicle = airplane;
-			myVehicle.Drive();
+			for (int i = 0; i < myMammals.Length; i++)
+			{
+				myMammals[i].Think();
+			}
 
-			myVehicle = car;
-			myVehicle.Drive();
+			Mammal myMammal = new Mammal("");
+			myMammal.Breath();
 		}
 	}
 
-	class Vehicle
+	class Mammal
 	{
-		public void StartEngine()
+		private string name;
+
+		public Mammal(string name)
 		{
-			Console.WriteLine("Starting engine...");
+			this.name = name;
 		}
 
-		public void StopEngine()
+		public void Breath()
 		{
-			Console.WriteLine("Stopping engine...");
+			Console.WriteLine("I can breathe");
 		}
 
-		public virtual void Drive()
+		public virtual void Think()
 		{
-			Console.WriteLine("Vehicle to drive...");
+			Console.WriteLine("Just Think");
+		}
+
+		public void Eat()
+		{
+			Console.WriteLine("I am eating");
+		}
+
+		public string getName()
+		{
+			return name;
 		}
 	}
 
-	class Airplane : Vehicle
+	class Horse : Mammal
 	{
-		public override void Drive()
+		// this calls the parent constructor ->  Mammal
+		public Horse(string horseName) : base(horseName) { }
+
+		public void Gallop()
 		{
-			Console.WriteLine("Piloting an Airplane.");
+			Console.WriteLine("I am able to gallop...");
 		}
 	}
 
-	class Car : Vehicle
+	class Human : Mammal
 	{
-		public override void Drive()
+		public Human(string humanName) : base(humanName) { }
+
+		public override void Think()
 		{
-			Console.WriteLine("Driving a Car.");
+			Console.WriteLine("Overthink");
 		}
+	}
+
+	class Gorilla : Mammal
+	{
+		public Gorilla(string gorillaName) : base(gorillaName) { }
+
+		public void Climb()
+		{
+			Console.WriteLine("I am climbing");
+		}
+
+		public override void Think()
+		{
+			Console.WriteLine("Think like a Gorilla");
+		}
+	}
+
+	class Whale : Mammal
+	{
+		public Whale(string name) : base(name) { }
+
+		public void Swim()
+		{
+            Console.WriteLine("I am able to swim");
+        }
 	}
 }
