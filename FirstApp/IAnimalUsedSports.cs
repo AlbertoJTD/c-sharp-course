4eb7d8f4e2e35e4e18fcb865bec7b6fe27dcdf91
AlbertoJTD <albertojtd@outlook.com>
2023-08-13 15:55:25 -0600

    Recover Mammal example

---
+++ FirstApp/IAnimalUsedSports.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FirstApp
+{
+	interface IAnimalUsedSports
+	{
+		string SportType();
+		bool IsOlimpic();
+	}
+}
