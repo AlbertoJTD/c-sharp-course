99d92324be6c4b7e50756601e5eae95ba2060751
AlbertoJTD <albertojtd@outlook.com>
2023-08-13 09:18:18 -0600

    Add example Vehicle

--- FirstApp/Program.cs
+++ FirstApp/Program.cs
@@ -6,108 +6,50 @@ namespace FirstApp
 	{
 		static void Main(string[] args)
 		{
-			Horse horse = new Horse("Mustang");
-			Human human = new Human("John Wick");
-			Gorilla gorilla = new Gorilla("Joe");
+			Airplane airplane = new Airplane();
+			Car car = new Car();
 
-			//gorilla.Climb();
+			airplane.StartEngine();
+			airplane.Drive();
+			airplane.StopEngine();
 
-			//Console.WriteLine(gorilla.getName());
-			//Console.WriteLine(horse.getName());
-			//Console.WriteLine(human.getName());
-
-			// ----- Substitucion principle -----
-			//Horse horse2 = new Mammal("Horse"); This is not possible
-
-			//Mammal animal = new Horse("Horse"); // This is correct
-			//Mammal person = new Human("Brayan"); // This is correct
-
-			//Mammal animal2 = new Mammal("Animal2");
-			//Horse horse2 = new Horse("Horse2");
-			//animal2 = horse2;
-
-			//Object myAnimal = new Horse("Tiger");
-			//Object myPerson = new Horse("Jonny");
-			//Object myMammal = new Horse("Wally");
-
-			//Mammal[] myMammals = new Mammal[3];
-			//myMammals[0] = animal;
-			//myMammals[1] = person;
-			//myMammals[2] = new Gorilla("King kong");
-
-			//for (int i = 0; i < myMammals.Length; i++)
-			//{
-			//	myMammals[i].Think();
-			//}
-
-			Mammal myMammal = new Mammal("");
-			myMammal.Breath(); // myMammal cannot access Breath() method
+			car.StartEngine();
+			car.Drive();
+			car.StopEngine();
 		}
 	}
 
-	class Mammal
+	class Vehicle
 	{
-		private string name;
-
-        public Mammal(string name)
-        {
-			this.name = name;
-        }
-
-        protected void Breath()
+		public void StartEngine()
 		{
-            Console.WriteLine("I can breathe");
-        }
-
-		public virtual void Think()
-		{
-			Console.WriteLine("Just Think");
+			Console.WriteLine("Starting engine...");
 		}
 
-		public void Eat()
+		public void StopEngine()
 		{
-            Console.WriteLine("I am eating");
-        }
+			Console.WriteLine("Stopping engine...");
+		}
 
-		public string getName()
+		public virtual void Drive()
 		{
-			return name;
+			Console.WriteLine("Vehicle to drive...");
 		}
 	}
 
-	class Horse : Mammal
+	class Airplane : Vehicle
 	{
-		// this calls the parent constructor ->  Mammal
-		public Horse(string horseName) : base(horseName) { }
-
-        public void Gallop()
+		public override void Drive()
 		{
-			Console.WriteLine("I am able to gallop...");
+			Console.WriteLine("Piloting an Airplane.");
 		}
 	}
 
-	class Human : Mammal
+	class Car : Vehicle
 	{
-        public Human(string humanName) : base(humanName) { }
-
-        public override void Think()
+		public override void Drive()
 		{
-			Console.WriteLine("Overthink");
-		}
-	}
-
-	class Gorilla : Mammal
-	{
-        public Gorilla(string gorillaName) : base(gorillaName) { }
-
-        public void Climb()
-		{
-			Console.WriteLine("I am climbing");
-		}
-
-		public override void Think()
-		{
-			Console.WriteLine("Think like a Gorilla");
+			Console.WriteLine("Driving a Car.");
 		}
 	}
 }
