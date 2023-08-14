125f7c0618fd4a99262a24f1fd5b79fce4055d42
AlbertoJTD <albertojtd@outlook.com>
2023-08-14 13:18:46 -0600

    Use properties

--- FirstApp/Program.cs
+++ FirstApp/Program.cs
@@ -8,7 +8,13 @@ namespace FirstApp
 		static void Main(string[] args)
 		{
 			Employee john = new Employee("John");
-		}
+			//john.SetSalary(1000);
+			//Console.WriteLine($"John's salary: {john.GetSalary()}");
+
+			john.SALARY = 1000;
+			john.SALARY += 200;
+            Console.WriteLine($"John's salary: {john.SALARY}");
+        }
 	}
 
 	class Employee
@@ -16,11 +22,24 @@ namespace FirstApp
 		private string name;
 		private double salary;
 
+        public double SALARY
+		{
+			get { return this.salary; }
+			set { this.salary = EvaluateSalary(value); }
+		}
+
         public Employee(string name)
         {
             this.name = name;
         }
 
+		private double EvaluateSalary(double salary)
+		{
+			if (salary < 0) return 0;
+			else return salary;
+		}
+
+		/*
 		public void SetSalary(double salary)
 		{
 			if (salary < 0)
@@ -33,5 +52,11 @@ namespace FirstApp
 				this.salary += salary;
             }
         }
-    }
+
+		public double GetSalary()
+		{
+			return this.salary;
+		}
+		*/
+	}
 }
