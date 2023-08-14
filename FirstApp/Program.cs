48b60d37011e152c08d2a29f8f8fda14d80236ac
AlbertoJTD <albertojtd@outlook.com>
2023-08-14 14:30:53 -0600

    Use struct

--- FirstApp/Program.cs
+++ FirstApp/Program.cs
@@ -8,12 +8,14 @@ namespace FirstApp
 		static void Main(string[] args)
 		{
 			Employee employee = new Employee(1200, 250);
+			employee.ModifySalary(employee, 100);
 
             Console.WriteLine(employee);
         }
 	}
 
-	class Employee
+	public struct Employee
+	//public class Employee
 	{
 		public double baseSalary, commission;
 
@@ -26,6 +28,12 @@ namespace FirstApp
 		public override string ToString()
 		{
 			return string.Format("Sallary and comission for employee ({0}, {1})", this.baseSalary, this.commission);
+		}
+
+		public void ModifySalary(Employee emp, double increase)
+		{
+			emp.baseSalary += increase;
+			emp.commission += increase;
 		}
     }
 }
