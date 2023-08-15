2b2592eec7fa8c89dc5f6f705424a95edf1da4a3
AlbertoJTD <albertojtd@outlook.com>
2023-08-15 15:25:51 -0600

    Add WarehouseObjects class

--- FirstApp/Program.cs
+++ FirstApp/Program.cs
@@ -10,4 +10,26 @@ namespace FirstApp
 
 		}
 	}
+
+	class WarehouseObjects
+	{
+		private Object[] data;
+		private int counter = 0;
+
+        public WarehouseObjects(int numberElements)
+        {
+			data = new Object[numberElements];
+        }
+
+		public void AddElement(Object obj)
+		{
+			data[counter++] = obj;
+			counter++;
+		}
+
+		public Object GetElement(int position)
+		{
+			return data[position];
+		}
+    }
 }
