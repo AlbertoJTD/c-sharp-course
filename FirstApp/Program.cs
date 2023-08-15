99d3acb564855ecd40fe5a027cae5460bf4eb1c7
AlbertoJTD <albertojtd@outlook.com>
2023-08-15 15:13:27 -0600

    Remove code

--- FirstApp/Program.cs
+++ FirstApp/Program.cs
@@ -7,37 +7,7 @@ namespace FirstApp
 	{
 		static void Main(string[] args)
 		{
-			ManageFiles manageFiles = new ManageFiles();
-			manageFiles.Message();
-		}
-	}
 
-	class ManageFiles
-	{
-		StreamReader file = null;
-		int counter = 0;
-		string line;
-
-		public ManageFiles()
-		{
-			file = new StreamReader(@"test_file.txt");
-
-			while ((line = file.ReadLine()) != null)
-			{
-				Console.WriteLine(line);
-				counter++;
-			}
-		}
-
-		// Destructor
-		~ManageFiles()
-		{
-			file.Close();
-		}
-
-		public void Message()
-		{
-			Console.WriteLine($"Number of lines: {counter}");
 		}
 	}
 }
