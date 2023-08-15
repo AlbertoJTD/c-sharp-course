18c596575172bdd10e2e57e7da3cf39342cc1599
AlbertoJTD <albertojtd@outlook.com>
2023-08-15 17:06:58 -0600

    Remove WarehouseObjects example

--- FirstApp/Program.cs
+++ FirstApp/Program.cs
@@ -7,58 +7,7 @@ namespace FirstApp
 	{
 		static void Main(string[] args)
 		{
-			WarehouseObjects<Employee> files = new WarehouseObjects<Employee>(4);
 
-			// ------------------
-			files.AddElement(new Employee(2000));
-			files.AddElement(new Employee(1000));
-			files.AddElement(new Employee(500));
-			files.AddElement(new Employee(100));
-
-			Employee person = files.GetElement(2);
-            Console.WriteLine(person.GetSalary());
-
-
-			WarehouseObjects<DateTime> dates = new WarehouseObjects<DateTime>(1);
-			dates.AddElement(new DateTime());
-            Console.WriteLine(dates.GetElement(0));
         }
 	}
-
-	class WarehouseObjects<T>
-	{
-		private T[] data;
-		private int counter = 0;
-
-        public WarehouseObjects(int numberElements)
-        {
-			data = new T[numberElements];
-        }
-
-		public void AddElement(T obj)
-		{
-			data[counter] = obj;
-			counter++;
-		}
-
-		public T GetElement(int position)
-		{
-			return data[position];
-		}
-    }
-
-	class Employee
-	{
-		private double salary;
-
-        public Employee(double salary)
-        {
-			this.salary = salary;
-        }
-
-		public double GetSalary()
-		{
-			return salary;
-		}
-    }
 }
