02a9c74bb34c4d3e0ec00324f01b53e0d0cad641
AlbertoJTD <albertojtd@outlook.com>
2023-08-16 14:04:29 -0600

    Remove delegate example

--- FirstApp/Program.cs
+++ FirstApp/Program.cs
@@ -7,40 +7,7 @@ namespace FirstApp
 	{
 		static void Main(string[] args)
 		{
-			/* Example 1
-			Create delegate object pointing to Greetings() */
-			//DelegateObject delegateObject = new DelegateObject(WelcomeMessage.Greetings);
-			DelegateObject delegateObject = new DelegateObject(WelcomeMessage.Greetings);
 
-			// Use delegate to call the method Greetings
-			//delegateObject();
-
-			delegateObject("C sharp");
-
-
-			/* Example 2
-			Create delegate object pointing to Bye() */
-			delegateObject = new DelegateObject(GoodbyeMessage.Bye);
-
-			// Use delegate to call the method Greetings
-			//delegateObject();
-			delegateObject("Butterfly");
 		}
-
-		// delegate object
-		//delegate void DelegateObject();
-		delegate void DelegateObject(string message);
-	}
-
-	class WelcomeMessage
-	{
-		public static void Greetings() => Console.WriteLine($"Hello world!");
-		public static void Greetings(string msj) => Console.WriteLine($"Hello world! -> {msj}");
-	}
-
-	class GoodbyeMessage
-	{
-		public static void Bye() => Console.WriteLine($"See you later!");
-		public static void Bye(string msj) => Console.WriteLine($"See you later! -> {msj}");
 	}
 }
