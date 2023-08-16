9f8363a95c0706ab7c583b9a37c488286956e65a
AlbertoJTD <albertojtd@outlook.com>
2023-08-15 21:12:09 -0600

    Stack

--- FirstApp/Program.cs
+++ FirstApp/Program.cs
@@ -7,24 +7,24 @@ namespace FirstApp
 	{
 		static void Main(string[] args)
 		{
-			Queue<int> numbers = new Queue<int>();
+			Stack<int> numbers = new Stack<int>();
 
             // Add elements to the Queue
             foreach (int num in new int[] { 1, 2, 3, 4, 5, 6, 7, 8 })
             {
-				numbers.Enqueue(num);
+				numbers.Push(num);
             }
 
             // Read Queue
-            Console.WriteLine("Read Queue:");
+            Console.WriteLine("Read Stack:");
             foreach (int num in numbers)
             {
                 Console.WriteLine(num);
             }
 
 			// Delete items from the queue
-			Console.WriteLine("\nDelete first element of the Queue:");
-			numbers.Dequeue();
+			Console.WriteLine("\nDelete element of the Stack:");
+			numbers.Pop();
 			foreach (int num in numbers)
 			{
 				Console.WriteLine(num);
