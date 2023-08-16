8eac1e093343d72dc1ff50f812b05cac45f3699f
AlbertoJTD <albertojtd@outlook.com>
2023-08-15 20:45:04 -0600

    Queue

--- FirstApp/Program.cs
+++ FirstApp/Program.cs
@@ -7,7 +7,28 @@ namespace FirstApp
 	{
 		static void Main(string[] args)
 		{
+			Queue<int> numbers = new Queue<int>();
 
-        }
+            // Add elements to the Queue
+            foreach (int num in new int[] { 1, 2, 3, 4, 5, 6, 7, 8 })
+            {
+				numbers.Enqueue(num);
+            }
+
+            // Read Queue
+            Console.WriteLine("Read Queue:");
+            foreach (int num in numbers)
+            {
+                Console.WriteLine(num);
+            }
+
+			// Delete items from the queue
+			Console.WriteLine("\nDelete first element of the Queue:");
+			numbers.Dequeue();
+			foreach (int num in numbers)
+			{
+				Console.WriteLine(num);
+			}
+		}
 	}
 }
