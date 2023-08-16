a7e62636cb51bb6e554098c90db169ccefec3599
AlbertoJTD <albertojtd@outlook.com>
2023-08-16 14:25:38 -0600

    Use predicate

--- FirstApp/Program.cs
+++ FirstApp/Program.cs
@@ -7,7 +7,22 @@ namespace FirstApp
 	{
 		static void Main(string[] args)
 		{
+			List<int> numbers = new List<int>();
+			numbers.AddRange(new int[] { 1, 2, 3, 4, 5, 6, 7, 8, 9, 10 });
 
-		}
+			// Create Predicate
+			Predicate<int> myPredicate = new Predicate<int>(EvenNumber);
+
+			// Store all numbers that match the predicate condition
+			List<int> evenNumbers =  numbers.FindAll(myPredicate);
+
+			// Print even numbers
+            foreach (int num in evenNumbers)
+            {
+                Console.WriteLine(num);
+            }
+        }
+
+		static bool EvenNumber(int number) => number % 2 == 0;
 	}
 }
