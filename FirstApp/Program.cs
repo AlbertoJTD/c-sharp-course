8d66d4093ab7e8a90ae7d5fcf20037905aeed010
AlbertoJTD <albertojtd@outlook.com>
2023-08-15 20:30:40 -0600

    Remove LinkedList

--- FirstApp/Program.cs
+++ FirstApp/Program.cs
@@ -7,26 +7,7 @@ namespace FirstApp
 	{
 		static void Main(string[] args)
 		{
-			LinkedList<int> list = new LinkedList<int>();
 
-            foreach (int num in new int[] { 1, 2, 3, 4, 5, 6, 7, 8, 9 })
-            {
-				list.AddFirst(num);
-            }
-
-            /*foreach (int num in list)
-            {
-                Console.WriteLine(num);
-            }*/
-
-            LinkedListNode<int> importantNode = new LinkedListNode<int>(15);
-            list.AddFirst(importantNode);
-
-            for (LinkedListNode<int> nodo = list.First; nodo != null; nodo = nodo.Next)
-            {
-                int num = nodo.Value;
-                Console.WriteLine(num);
-            }
         }
 	}
 }
