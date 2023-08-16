e1bba7b6718cd1e9731acd19fc53d12b044f0a96
AlbertoJTD <albertojtd@outlook.com>
2023-08-16 13:01:30 -0600

    Remove dictionary example

--- FirstApp/Program.cs
+++ FirstApp/Program.cs
@@ -7,19 +7,7 @@ namespace FirstApp
 	{
 		static void Main(string[] args)
 		{
-			Dictionary<string, int> people = new Dictionary<string, int>();
 
-			// Add elements
-			people.Add("John", 35);
-			people.Add("Elena", 39);
-			people.Add("Jax", 34);
-			people["Cassy"] = 29;
-
-            // Read dictionary
-            foreach (KeyValuePair<string, int> kvp in people)
-            {
-                Console.WriteLine($"Person: ({kvp.Key}, {kvp.Value})");
-            }
         }
 	}
 }
