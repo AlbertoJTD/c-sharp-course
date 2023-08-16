cde784229dd0e452b379c10b27d55fffa3bcc0bc
AlbertoJTD <albertojtd@outlook.com>
2023-08-15 21:24:53 -0600

    Remove stack example

--- FirstApp/Program.cs
+++ FirstApp/Program.cs
@@ -7,28 +7,7 @@ namespace FirstApp
 	{
 		static void Main(string[] args)
 		{
-			Stack<int> numbers = new Stack<int>();
 
-            // Add elements to the Queue
-            foreach (int num in new int[] { 1, 2, 3, 4, 5, 6, 7, 8 })
-            {
-				numbers.Push(num);
-            }
-
-            // Read Queue
-            Console.WriteLine("Read Stack:");
-            foreach (int num in numbers)
-            {
-                Console.WriteLine(num);
-            }
-
-			// Delete items from the queue
-			Console.WriteLine("\nDelete element of the Stack:");
-			numbers.Pop();
-			foreach (int num in numbers)
-			{
-				Console.WriteLine(num);
-			}
 		}
 	}
 }
